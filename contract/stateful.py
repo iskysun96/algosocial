02322fd42c471bff188b966f70833ba2bd698091
Chris Kim <52557585+iskysun96@users.noreply.github.com>
2022-06-30 18:29:27 -0700

    added follower, following functionalities

--- contract/stateful.py
+++ contract/stateful.py
@@ -21,15 +21,16 @@ def approval_program():
     twitter = Bytes("twitter")
     donation = Bytes("donation")
     follower = Bytes("follower")
+    following = Bytes("following")
     joined = Bytes("joined")
 
     handle_creation = Seq(
         Assert(
             And(
-                Txn.application_args.length() == Int(8),
+                Txn.application_args.length() == Int(6),
                 Txn.application_args[2] == TealType.bytes,
                 Txn.application_args[3] == TealType.uint64,
-            )
+            ),
         ),
         App.globalPut(name, Txn.application_args[0]),
         App.globalPut(tag, Txn.application_args[1]),
@@ -39,56 +40,92 @@ def approval_program():
         App.globalPut(twitter, Txn.application_args[5]),
         App.globalPut(donation, Int(0)),
         App.globalPut(follower, Int(0)),
+        App.globalPut(following, Int(0)),
         App.globalPut(joined, Global.latest_timestamp()),
         Approve(),
     )
 
-    # @Subroutine(TealType.none)
-    # def withdraw(account: Expr, amount: Expr) -> Expr:
-    #     return Seq(
-    #         InnerTxnBuilder.Begin(),
-    #         InnerTxnBuilder.SetFields(
-    #             {
-    #                 TxnField.type_enum: TxnType.Payment,
-    #                 TxnField.amount: amount,
-    #                 TxnField.receiver: account
-    #             }
-    #         ),
-    #         InnerTxnBuilder.Submit(),
-    #     )
+    handle_optin = Seq(
+        # group txn. 
+        # 1st: app cal the txn sender's contract to update their following count (on_following_update)
+        # 2nd: update follower of this contract 
+        # another solution
+        # pass in app ID and then do inner transaction 
+        # application args[0] = caller's app ID
+        Assert(
+            And(
+                Txn.sender() != App.globalGet(wallet_addr),
+                #compare hash of profile A and B TODO: change below code when nullun post guide
+                AppParam.approvalProgram(Txn.applications[1]) == AppParam.approvalProgram(approval_program()),
+            ),
+        ),
+        App.globalPut(follower, App.globalGet(follower) + Int(1)),
+        Approve(),
+    )
 
-    # @Subroutine(TealType.none)
-    # def donate(amount: Expr) -> Expr:
-    #     return Seq(
-    #         InnerTxnBuilder.Begin(),
-    #         InnerTxnBuilder.SetFields(
-    #             {
-    #                 TxnField.type_enum: TxnType.Payment,
-    #                 TxnField.amount: amount,
+    on_follow = Seq(
+        Assert(
+                Txn.application_args.length() == Int(2),
+        ),
+        App.globalPut(following, App.globalGet(following) + Int(1)),
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields(
+            {
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.on_completion: OnComplete.OptIn,
+                TxnField.receiver: Txn.application_args[1]
+            }
+        ),
+        InnerTxnBuilder.Submit(),
+        Approve(),
+    )
 
-    #             }
-    #         )
-    #     )
+    handle_closeout = Seq(
+        Assert(
+            And(
+                Txn.sender() != App.globalGet(wallet_addr),
+                #TODO: change this code when nullun post guide
+                AppParam.approvalProgram(Txn.applications[1]) == AppParam.approvalProgram(approval_program()),
+            )
+        ),
+        App.globalPut(follower, App.globalGet(follower) - Int(1)), 
+        Approve(),
+    )
 
-    handle_optin = Return(Int(1))
-
-    handle_closeout = Return(Int(1))
-
-    handle_deleteapp = Return(Int(1))
+    on_unfollow = Seq(
+        Assert(
+            Txn.application_args.length() == Int(2),
+        ),
+        App.globalPut(following, App.globalGet(following) - Int(1)),
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields(
+            {
+                TxnField.type_enum: TxnType.ApplicationCall,
+                TxnField.on_completion: OnComplete.CloseOut,
+                TxnField.receiver: Txn.application_args[1]
+            }
+        ),
+        InnerTxnBuilder.Submit(),
+        Approve(),
+    )
 
     on_call_method = Txn.application_args[0]
     on_call = Cond(
+        # Five conditions for NoOp
         [on_call_method == Bytes("update_info"), on_update_info],
-        [on_call_method == Bytes("follower"), on_follower_update],
         [on_call_method == Bytes("withdraw"), on_withdraw],
         [on_call_method == Bytes("donate"), on_donate],
+        [on_call_method == Bytes("follow"), on_follow],
+        [on_call_method == Bytes("unfollow"), on_unfollow],
     )
 
     on_update_info = Seq([
         # only profile owner can call this function
         Assert(
-            Txn.sender() == App.globalGet("WalletAddr"),
-            Txn.application_args.length() == Int(1),
+            And(
+                Txn.sender() == App.globalGet("WalletAddr"),
+                Txn.application_args.length() == Int(1),
+            )
         ),
         If(Txn.application_args[0] == name).Then(
             App.globalPut(name, Txn.application_args[1]),
@@ -120,20 +157,6 @@ def approval_program():
         Approve(),
     ])
 
-    on_follower_update = Seq(
-        "followers update (TODO)",
-        Assert(
-            Txn.sender() != App.globalGet(wallet_addr),
-        ),
-        If(Txn.application_args[0] == Bytes("follow")).Then(
-            App.globalPut(follower, App.globalGet(follower) + Int(1)),
-        ),
-        If(Txn.application_args[0] == Bytes("unfollow")).Then(
-            App.globalPut(follower, App.globalGet(follower) - Int(1)),
-        ), 
-        Approve(),
-    )
-
     on_donate_txn_index = Txn.group_index() - Int(1)
 
     on_donate = Seq(
@@ -143,9 +166,11 @@ def approval_program():
         # 2nd: app call
 
         Assert(
-            Gtxn[on_donate_txn_index].type_enum() == TxnType.Payment,
-            Gtxn[on_donate_txn_index].receiver() == Global.current_application_address(),
-            Gtxn[on_donate_txn_index].amount() >= Global.min_txn_fee(),
+            And(
+                Gtxn[on_donate_txn_index].type_enum() == TxnType.Payment,
+                Gtxn[on_donate_txn_index].receiver() == Global.current_application_address(),
+                Gtxn[on_donate_txn_index].amount() >= Global.min_txn_fee(),
+            ),
         ),
         App.globalPut(donation, App.globalGet(donation) + Gtxn[on_donate_txn_index].amount()),
         Approve(),
@@ -164,10 +189,12 @@ def approval_program():
         donation_amt_global,
         If(App.globalGet(donation_amt_global) > Int(0)).Then(
             Assert(
-                Gtxn[on_donate_txn_index].sender() == App.globalGet(wallet_addr),
-                Gtxn[on_donate_txn_index].type_enum() == TxnType.Payment,
-                Gtxn[on_donate_txn_index].receiver() == wallet_global,
-                Gtxn[on_donate_txn_index].amount() <= donation_amt_global,
+                And(
+                    Gtxn[on_donate_txn_index].sender() == App.globalGet(wallet_addr),
+                    Gtxn[on_donate_txn_index].type_enum() == TxnType.Payment,
+                    Gtxn[on_donate_txn_index].receiver() == wallet_global,
+                    Gtxn[on_donate_txn_index].amount() <= donation_amt_global,
+                ),
             ),
             App.globalPut(donation, donation_amt_global - Gtxn[on_donate_txn_index].amount()),
             Approve(),
@@ -175,6 +202,8 @@ def approval_program():
         Reject(),
     )
 
+    handle_updateapp = Reject()
+    handle_deleteapp = Return(Int(1))
         
     program = Cond(
         [Txn.application_id() == Int(0), handle_creation],
